81be4a9af4a8ac638158c10165766f021920098d
phung <phung1470@gmail.com>
2017-03-08 23:53:32 +0700

    services - raw commit

---
+++ exponet/js/custom.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $('.slick-client').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        centerMode: true,
+        lazyLoad: 'progressive',
+        infinite: true,
+        variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    variableWidth: true
+                }
+            },
+            {
+                breakpoint: 360,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    variableWidth: true
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+    $('.testimonial').slick({
+        autoplay: true,
+        autoplaySpeed: 10000
+    });
+});
